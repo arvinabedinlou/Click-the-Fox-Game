3674b6e92ac5e2c2c7537269a57a5938352148d4
Arvin <abedinlouarvin@gmail.com>
2023-02-09 07:32:21 +0330

    change data fetching in game screen

--- src/view/game-screen2/index.tsx
+++ src/view/game-screen2/index.tsx
@@ -11,102 +11,72 @@ const Index = () => {
   const [dogs, setDogs] = useState<any>([]);
   const [cats, setCats] = useState<any>([]);
   const [fox, setFox] = useState<any>({});
-  const [imgsLoaded, setImgsLoaded] = useState(false);
+  // const [imgsLoaded, setImgsLoaded] = useState(false);
   const [timeStart, setTimeStart] = useState<boolean>(false);
   const [page, setPage] = useState<number>(0);
   const [scoreItem, setScoreItem] = useState<any>({});
   const [showLoading, setShowLoading] = useState<boolean>(true);
-  const [imageCountLoaded, setImageCountLoaded] = useState<number>(0);
-
+  const [totalPictures, setTotalPictures] = useState<any>([]);
   // loadings wa handled in second useEffect
 
-  useEffect(() => {
-    // const getPictures = () => {
-    // setImgsLoaded(false);
-    PicturesService.getDogsList({
-      showMessage(message) {},
-      onSuccess(data) {
-        let typedDogs = addType(data, "dog");
-        setDogs([...typedDogs]);
-      },
-    });
-    PicturesService.getCatsList({
-      showMessage(message) {},
-      onSuccess(data) {
-        let typedCats = addType(data, "cat");
-        setCats([...typedCats]);
-      },
-    });
-    PicturesService.getFoxItem({
-      showMessage(message) {},
-      onSuccess(data) {
-        delete Object.assign(data, { url: data.image })["image"];
-        setFox(data);
-      },
-    });
-  }, [page]);
+  // useEffect(() => {
+
+  //   PicturesService.getDogsList({
+  //     showMessage(message) {},
+  //     onSuccess(data) {
+  //       let typedDogs = addType(data, "dog");
+  //       setDogs([...typedDogs]);
+  //     },
+  //   });
+  //   PicturesService.getCatsList({
+  //     showMessage(message) {},
+  //     onSuccess(data) {
+  //       let typedCats = addType(data, "cat");
+  //       setCats([...typedCats]);
+  //     },
+  //   });
+  //   PicturesService.getFoxItem({
+  //     showMessage(message) {},
+  //     onSuccess(data) {
+  //       delete Object.assign(data, { url: data.image })["image"];
+  //       setFox(data);
+  //     },
+  //   });
+  // }, [page]);
 
   // mix and shuffle data
 
-  let mixPictures = [...dogs, ...cats, fox];
-  const shuffledPictures = mixPictures.sort((a, b) => 0.5 - Math.random());
+  // let mixPictures = [...dogs, ...cats, fox];
+  // const shuffledPictures = mixPictures.sort((a, b) => 0.5 - Math.random());
 
-  //disabling view before images loading
-  // const Images = (urls: any) => {
-  //   const onComplete = after(shuffledPictures.length, () => {
-  //     setLoading(false);
-  //     console.log("loaded");
-  //   });
-
-  // function Images({ shuffledPictures }: any) {
-  //   const onComplete = after(shuffledPictures?.length, () => {
-  //     // setLoading(false);
-  //     console.log("loaded");
-  //   });
-  // }
-
-  const onLoad: any = after(9, () => {
+  const onLoad: any = after(1, () => {
     console.log("loaded");
   });
-  useEffect(() => {
-    // const loadImage = (image: any) => {
-    //   return new Promise((resolve, reject) => {
-    //     const loadImg = new Image();
-    //     loadImg.src = image.url;
-    //     loadImg.onload = () => {
-    //       resolve(image.url);
-    //       loadImg.onerror = (err) => reject(err);
-    //     };
-    //   });
-    // };
-    // Promise.all(shuffledPictures.map((image: any) => loadImage(image)))
-    //   .then(() => {
-    //     console.log("12");
-    //     setImgsLoaded(true);
-    //     setShowLoading(false);
-    //     setTimeStart(true);
-    //   })
-    //   .catch((err) => console.log("Failed to load images", err));
-  });
 
-  // const onLoad = () => {
-  // setImageCountLoaded(imageCountLoaded + 1);
-  // if (imageCountLoaded === 9) {
-  // setImgsLoaded(true);
-  // setShowLoading(false);
-  // setTimeStart(true);
-  // }
-  // const onLoad: any = after(9, () => {
-  //   setImgsLoaded(true);
-  //   setShowLoading(false);
-  //   setTimeStart(true);
-  //   console.log("all done");
-  // });
-  // after(shuffledPictures?.length, () => {
-  //   setLoading(false);
-  //   console.log("loaded1");
-  // });
-  // };
+  const [data, setData] = useState({ recentWo: null, woCount: null });
+
+  const fetchData = async () => {
+    //  const api = new ApiService();
+
+    const recentWo = await PicturesService.getDogsList({
+      showMessage(message) {},
+      onSuccess(data) {
+        let typedDogs = addType(data, "dog");
+        setTotalPictures([...totalPictures, ...typedDogs]);
+      },
+    });
+    const woCount = await PicturesService.getCatsList({
+      showMessage(message) {},
+      onSuccess(data) {
+        let typedCats = addType(data, "cat");
+        setTotalPictures([...totalPictures, ...typedCats]);
+      },
+    });
+    setShowLoading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
@@ -120,10 +90,10 @@ const Index = () => {
               />
               <Score item={scoreItem} />
             </SizedBox>
-            {imgsLoaded === true && showLoading === false ? (
+            {showLoading === false ? (
               <SizedBox>
                 <div className="grid">
-                  {shuffledPictures?.map((item: any, index: any) => {
+                  {totalPictures?.map((item: any, index: any) => {
                     return (
                       <div key={index}>
                         {" "}
