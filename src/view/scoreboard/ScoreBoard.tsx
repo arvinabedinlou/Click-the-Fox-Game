670efba414fb6faa9ca12d92655e67f11b44c9a9
Arvin <abedinlouarvin@gmail.com>
2023-02-10 23:58:24 +0330

    change scoreboard table getting data

--- src/view/scoreboard/ScoreBoard.tsx
+++ src/view/scoreboard/ScoreBoard.tsx
@@ -2,18 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import Column from "../../components/ui/column/Column";
 import SizedBox from "../../components/ui/sized-box/SizedBox";
-import useDebounce from "../../utils/debounce";
 import "./index.css";
 
 const ScoreBoard: React.FC = () => {
   const [results, setResults] = useState<any>([]);
   const playerResult = useLocation();
   console.log(results);
-
-  setResults((prevstate: any) => {
-    return [...prevstate, { ...playerResult.state.playerData }];
-  });
-
+  useEffect(() => {
+    setResults((prevState: any) => {
+      return [...prevState, { ...playerResult.state.playerData }];
+    });
+  }, [playerResult.state.playerData]);
   return (
     <>
       <Column>
@@ -28,10 +27,20 @@ const ScoreBoard: React.FC = () => {
                 <th>Score</th>
               </tr>
               <tr>
-                <td>1</td>
+                {results.map((item: any, index: any) => {
+                  return (
+                    <div key={index}>
+                      <td>{index + 1}</td>
+                      <td>{item.playerInfo.state?.name}</td>
+                      <td>{item.playerInfo.state?.formattedDate}</td>
+                      <td>{item.score}</td>
+                    </div>
+                  );
+                })}
+                {/* <td>1</td>
                 <td>Alfreds Futterkiste</td>
                 <td>Maria Anders</td>
-                <td>Germany</td>
+                <td>Germany</td> */}
               </tr>
             </table>
           </Column>
