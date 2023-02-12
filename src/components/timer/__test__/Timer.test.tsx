53c94387d75809ff7ac0322fd3db355360d360cc
ArvinAbedinlou <abedinlouarvin@gmail.com>
2023-02-12 10:43:17 +0330

    Add new test scenarios to Score and Timer components

--- src/components/timer/__test__/Timer.test.tsx
+++ src/components/timer/__test__/Timer.test.tsx
@@ -3,14 +3,49 @@ import { screen, render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Timer from "../Timer";
 
-// jest.setTimeout(30000);
-test("Check the Timer is stop with passing false", () => {
-  render(
-    <BrowserRouter>
-      <Timer startTime={false} />
-    </BrowserRouter>
-  );
+describe("Check Timer component actions", () => {
+  test("Check the Timer is stop with passing false", () => {
+    render(
+      <BrowserRouter>
+        <Timer startTime={true} />
+      </BrowserRouter>
+    );
+    const divElement = screen.getByText(/Time Left :30/i);
+    expect(divElement).toBeInTheDocument();
+  });
 
-  const divElement = screen.getByText(/Time Left :30/i);
-  expect(divElement).toBeInTheDocument();
+  jest.setTimeout(30000);
+
+  test("check the timer will run if pass start time={true} , with 1 second delay", async () => {
+    render(
+      <BrowserRouter>
+        <Timer startTime={true} />
+      </BrowserRouter>
+    );
+    await new Promise((r) => setTimeout(r, 2000));
+    const divElement = screen.getByText(/Time Left :29/i);
+    expect(divElement).toBeInTheDocument();
+  });
+
+  test("check the timer will *not run if pass start time={false} , with 2 second delay", async () => {
+    render(
+      <BrowserRouter>
+        <Timer startTime={false} />
+      </BrowserRouter>
+    );
+    await new Promise((r) => setTimeout(r, 3000));
+    const divElement = screen.getByText(/Time Left :30/i);
+    expect(divElement).toBeInTheDocument();
+  });
+
+  test("check the timer will run and renders: 20 if pass start time={true} , with 10 second delay", async () => {
+    render(
+      <BrowserRouter>
+        <Timer startTime={true} />
+      </BrowserRouter>
+    );
+    await new Promise((r) => setTimeout(r, 11000));
+    const divElement = screen.getByText(/Time Left :20/i);
+    expect(divElement).toBeInTheDocument();
+  });
 });
